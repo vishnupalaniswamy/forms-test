d416e83f6914e78fcddabfe9834471b9d119a473
Vishnu Palaniswamy <vishnupalaniswamy@gmail.com>
2018-05-30 22:36:25 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Customer } from './customer.interface';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnChanges {
+    public myForm: FormGroup;
+
+    constructor(private _fb: FormBuilder) { }
+
+    ngOnInit() {
+        console.log('creating parent form group');
+        this.myForm = this._fb.group({
+            name: ['harry', [Validators.required, Validators.minLength(5), FormValidators.validateAll]],
+            addresses: this._fb.array([
+                this.initAddress(),
+            ])
+        });
+        console.log('created form groups');
+        this.onChanges();
+
+        this.myForm.patchValue({
+            name: 'John'
+        });
+
+        this.myForm.controls.name.setValue('Joe');
+    }
+
+    onChanges(): void {
+        this.myForm.valueChanges.subscribe(val => {
+            console.log('onChanges', val);
+        });
+    }
+
+    onClick() {
+        console.log('onclick');
+    }
+
+    initAddress() {
+        console.log('creating child form group');
+        return this._fb.group({
+            street: ['street', Validators.required],
+            postcode: ['postcode']
+        });
+    }
+
+    addAddress() {
+        const control = <FormArray>this.myForm.controls['addresses'];
+        control.push(this.initAddress());
+    }
+
+    removeAddress(i: number) {
+        const control = <FormArray>this.myForm.controls['addresses'];
+        control.removeAt(i);
+    }
+
+    save(model: Customer) {
+        // call API to save
+        // ...
+        console.log(model);
+    }
+}
+
+class FormValidators {
+
+    static validateAll(control: AbstractControl): ValidationErrors | null {
+        console.log('validateAll ' + control.value);
+        return null;
+    }
+
 }
